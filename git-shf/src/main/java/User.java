784439a1998720e3b8328e05676b4982e1fb7595
唐泽成 <2065018335@qq.com>
2022-10-17 19:22:27 +0800

    第一次提交

---
+++ git-shf/src/main/java/User.java
@@ -0,0 +1,16 @@
+/**
+ * @author 11
+ */
+public class User {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
